284f0b6d62e4e3475f59b748f7cea2acc2562d4d
Levvroy <levvrae@gmail.com>
2026-05-19 02:01:58 +0700

    refactor: nambahin retractall dan assertz untuk arahPermainan dan kartuTersembunyi

--- src/startgame.pl
+++ src/startgame.pl
@@ -6,28 +6,63 @@ startGame :-
     retractall(kartudiTangan(_,_)),
     retractall(kartuTeratas(_,_)),
     retractall(warnaActive(_)),
+    retractall(tumpukan_kartu(_)),
     retractall(sudahMainKartu(_)),
     retractall(statusUni(_)),
     retractall(penantangWDF(_)),
-    assertz(sudahMainKartu(false)),
-    assertz(statusUni([])),
+    retractall(arahPermainan(_)),
+    retractall(kartuTersembunyi(_,_)),
     get_jumlah_pemain(N),
     get_nama_pemain(N, ListPemainRaw),
     nl,
+    buatDeckLengkap(DeckAwal),
+    acak_list(DeckAwal, DeckAcak),
+    bagiKartuSemua(ListPemainRaw, DeckAcak, DeckSisa),
     acak_list(ListPemainRaw, ListPemain),
     assertz(giliran(ListPemain)),
+    assertz(sudahMainKartu(false)),
+    assertz(statusUni([])),
+    assertz(penantangWDF(none)),
+    assertz(arahPermainan(kanan)),
     write('Urutan pemain: '), cetak_urutan(ListPemain), write('.'), nl, nl,
-    bagi_kartu_semua(ListPemain),
     write('Setiap pemain mendapatkan 7 kartu acak.'), nl, nl,
-    init_discard_pile,
+    initDiscardPile(DeckSisa, DeckFinal),
+    assertz(tumpukan_kartu(DeckFinal)),
     ListPemain = [PemainPertama|_],
     write('Giliran '), write(PemainPertama), write('.'), nl, !.
+
+buatDeckLengkap(Deck) :-
+    findall(kartu(W,J), kartu(W,J), Deck).
+
+bagiKartuSemua([], Deck, Deck).
+bagiKartuSemua([Pemain|SisaPemain], DeckAwal, DeckAkhir) :-
+    ambilNKartu(7, DeckAwal, Tangan, DeckSisa),
+    assertz(kartudiTangan(Pemain, Tangan)),
+    bagiKartuSemua(SisaPemain, DeckSisa, DeckAkhir).
+
+ambilNKartu(0, Deck, [], Deck) :- !.
+ambilNKartu(N, [Kartu|Sisa], [Kartu|Tangan], DeckAkhir) :-
+    N > 0,
+    N1 is N - 1,
+    ambilNKartu(N1, Sisa, Tangan, DeckAkhir).
+
+initDiscardPile([kartu(W,J)|Sisa], Sisa) :-
+    \+ is_action_card(J), !,
+    assertz(kartuTeratas(W, J)),
+    assertz(warnaActive(W)),
+    write('Kartu discard top: '), write(W), write('-'), write(J), write('.'), nl, nl.
+
+initDiscardPile([Kartu|Sisa], DeckFinal) :-
+    append_element(Sisa, Kartu, DeckBaru),
+    initDiscardPile(DeckBaru, DeckFinal).
 
 cetak_urutan([H]) :- write(H), !.
 cetak_urutan([H|T]) :- write(H), write(' - '), cetak_urutan(T).
 
 panjang_list([], 0).
-panjang_list([_|T], N) :- panjang_list(T, N1), N is N1 + 1.
+panjang_list([_|T], N) :-
+    panjang_list(T, N1),
+    N is N1 + 1.
 
 ambil_elemen_ke(0, [H|T], H, T) :- !.
 ambil_elemen_ke(N, [H|T], X, [H|Sisa]) :-
@@ -42,35 +77,6 @@ acak_list(List, [X|ListAcak]) :-
     ambil_elemen_ke(Index, List, X, SisaList),
     acak_list(SisaList, ListAcak).
 
-random_kartu(Warna, Jenis) :-
-    findall(kartu(W, J), kartu(W, J), SemuaKartu),
-    panjang_list(SemuaKartu, Len),
-    random(0, Len, Index),
-    ambil_elemen_ke(Index, SemuaKartu, kartu(Warna, Jenis), _).
-
-bagi_kartu_semua([]).
-bagi_kartu_semua([Pemain|T]) :-
-    bagi_kartu_pemain(7, Pemain, []),
-    bagi_kartu_semua(T).
-
-bagi_kartu_pemain(0, Pemain, Tangan) :-
-    assertz(kartudiTangan(Pemain, Tangan)), !.
-bagi_kartu_pemain(N, Pemain, Acc) :-
-    N > 0,
-    random_kartu(W, J),
-    NextN is N - 1,
-    bagi_kartu_pemain(NextN, Pemain, [kartu(W,J)|Acc]).
-
-init_discard_pile :-
-    random_kartu(W, J),
-    ( is_action_card(J) ->
-        init_discard_pile
-    ;
-        assertz(kartuTeratas(W, J)),
-        assertz(warnaActive(W)),
-        write('Kartu discard top: '), write(W), write('-'), write(J), write('.'), nl, nl
-    ).
-    
 is_action_card(skip).
 is_action_card(reverse).
 is_action_card(drawTwo).
