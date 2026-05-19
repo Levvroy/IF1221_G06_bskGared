b107ec3671cc547fbf5991e57813e792f2aa87da
Muhammad Rafiif Ansyadya <muhammadrafiifansyadya@gmail.com>
2026-05-19 11:31:09 +0700

    feat : add consult to endgame

--- src/main.pl
+++ src/main.pl
@@ -1,7 +1,5 @@
-% nanti masukin consult masing" file yang udah klean bkin ygy, jgn lupa di update terus main.pl nya 
-% ~ lewi
 
-:- consult('startgame.pl').
-:- consult('turn.pl').
-:- consult('poin.pl').
-:- consult('endGame.pl').
+:- include('startgame.pl').
+:- include('turn.pl').
+:- include('poin.pl').
+:- include('endGame.pl').
