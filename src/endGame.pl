e8f0b7c6f11fde479a432efe91e18b07327a0ae4
Muhammad Rafiif Ansyadya <muhammadrafiifansyadya@gmail.com>
2026-05-19 07:10:06 +0700

    chore : fix code at endgame.pl

--- src/endGame.pl
+++ src/endGame.pl
@@ -1,18 +1,40 @@
-urutPemain(Urutan, Order, P1, P2) :-
+posisiDalam(Elem, [Elem|_], 0) :- !.
+posisiDalam(Elem, [_|T], Idx) :-
+    posisiDalam(Elem, T, Idx0),
+    Idx is Idx0 + 1.
+
+tanpaPemain(_, [], []).
+tanpaPemain(P, [P|T], Hasil) :- !, tanpaPemain(P, T, Hasil).
+tanpaPemain(P, [H|T], [H|Hasil]) :- tanpaPemain(P, T, Hasil).
+
+urutKecil(Urutan, P1, P2) :-
     kartudiTangan(P1, T1), totalPoinKartu(T1, Poin1), length(T1, J1),
     kartudiTangan(P2, T2), totalPoinKartu(T2, Poin2), length(T2, J2),
-    nth0(Idx1, Urutan, P1),
-    nth0(Idx2, Urutan, P2),
-    (   Poin1 < Poin2 -> Order = (<)
-    ;   Poin1 > Poin2 -> Order = (>)
-    ;   J1    < J2    -> Order = (<)
-    ;   J1    > J2    -> Order = (>)
-    ;   Idx1  < Idx2  -> Order = (<)
-    ;                    Order = (>)
+    posisiDalam(P1, Urutan, Idx1),
+    posisiDalam(P2, Urutan, Idx2),
+    (   Poin1 < Poin2  -> true
+    ;   Poin1 =:= Poin2, J1 < J2 -> true
+    ;   Poin1 =:= Poin2, J1 =:= J2, Idx1 < Idx2
     ).
 
+insertSorted(_, P, [], [P]).
+insertSorted(Urutan, P, [H|T], [P,H|T]) :-
+    urutKecil(Urutan, P, H), !.
+insertSorted(Urutan, P, [H|T], [H|Hasil]) :-
+    insertSorted(Urutan, P, T, Hasil).
+
+insertionSort(_, [], []).
+insertionSort(Urutan, [H|T], Sorted) :-
+    insertionSort(Urutan, T, SortedT),
+    insertSorted(Urutan, H, SortedT, Sorted).
+
+printSemuaPoin([]).
+printSemuaPoin([P|T]) :-
+    printDetailPoin(P),
+    printSemuaPoin(T).
+
 cetakRanking([], _).
-cetakRanking([P | Sisa], Rank) :-
+cetakRanking([P|Sisa], Rank) :-
     kartudiTangan(P, Tangan),
     totalPoinKartu(Tangan, Poin),
     write(Rank), write('. '), write(P),
@@ -22,16 +44,15 @@ cetakRanking([P | Sisa], Rank) :-
 
 endGame(Pemenang) :-
     giliran(Urutan),
-    findall(P, kartudiTangan(P, _), SemuaPemain),
+    tanpaPemain(Pemenang, Urutan, Lainnya),
 
     write('Permainan selesai! '), write(Pemenang),
     write(' menghabiskan semua kartunya!'), nl, nl,
 
     write('Berikut perhitungan poin sisa kartu:'), nl,
-    maplist(cetakDetailPoin, Urutan), nl,
+    printSemuaPoin(Urutan), nl,
 
-    findall(P, (member(P, SemuaPemain), P \= Pemenang), Lainnya),
-    predsort(urutPemain(Urutan), Lainnya, LainnyaTerurut),
+    insertionSort(Urutan, Lainnya, LainnyaTerurut),
 
     write('Urutan pemenang:'), nl,
     cetakRanking([Pemenang | LainnyaTerurut], 1), nl,
