3565c725eaaf169128eac12fbe821bf6b680f160
Muhammad Rafiif Ansyadya <muhammadrafiifansyadya@gmail.com>
2026-05-30 12:56:52 +0700

    chore : edit bug dan sesuaikan dengan spek

--- src/loadGame.pl
+++ src/loadGame.pl
@@ -1,5 +1,3 @@
-% loadGame
-
 :- dynamic(giliran/1).
 :- dynamic(kartudiTangan/2).
 :- dynamic(kartuTeratas/2).
@@ -8,8 +6,11 @@
 :- dynamic(arahPermainan/1).
 :- dynamic(penantangWDF/1).
 :- dynamic(sudahMainKartu/1).
+:- dynamic(statusBluffWDF/1).
 :- dynamic(tumpukan_kartu/1).
 :- dynamic(kartuTersembunyi/2).
+:- dynamic(aksiTerakhir/4).
+:- dynamic(giliranKe/1).
 :- dynamic(giliranAktifTemp/1).
 
 reverse_list(List, Reversed) :- reverse_helper(List, [], Reversed).
@@ -39,10 +40,11 @@ buatDeckLengkap(Deck) :-
     semuaWarna(Warna), semuaJenis(Jenis),
     buatKartuBerwarna(Warna, Jenis, KartuBerwarna),
     KartuHitam = [
-        kartu(hitam, wild), kartu(hitam, wild),
-        kartu(hitam, wild), kartu(hitam, wild),
+        kartu(hitam, wild),         kartu(hitam, wild),
+        kartu(hitam, wild),         kartu(hitam, wild),
         kartu(hitam, wildDrawFour), kartu(hitam, wildDrawFour),
-        kartu(hitam, wildDrawFour), kartu(hitam, wildDrawFour)
+        kartu(hitam, wildDrawFour), kartu(hitam, wildDrawFour),
+        kartu(hitam, mimic)
     ],
     gabungList(KartuBerwarna, KartuHitam, Deck).
 
@@ -52,11 +54,14 @@ buatKartuBerwarna([W|RestW], Jenis, Hasil) :-
     buatKartuBerwarna(RestW, Jenis, RestHasil),
     gabungList(KartuW, RestHasil, Hasil).
 
-buatSatuWarna(W, [], []).
+buatSatuWarna(_, [], []).
 buatSatuWarna(W, [J|RestJ], Hasil) :-
     buatSatuWarna(W, RestJ, RestHasil),
-    (integer(J), J =:= 0 -> gabungList([kartu(W,J)], RestHasil, Hasil)
-    ; gabungList([kartu(W,J), kartu(W,J)], RestHasil, Hasil)).
+    (integer(J), J =:= 0 ->
+        gabungList([kartu(W,J)], RestHasil, Hasil)
+    ;
+        gabungList([kartu(W,J), kartu(W,J)], RestHasil, Hasil)
+    ).
 
 kumpulkanTerpakai(ListPemain, Terpakai) :-
     kumpulkanTangan(ListPemain, KartuTangan),
@@ -77,7 +82,63 @@ buatSisaDeck([K|Rest], Terpakai, Hasil) :-
 buatSisaDeck([K|Rest], Terpakai, [K|Hasil]) :-
     buatSisaDeck(Rest, Terpakai, Hasil).
 
-% Baca File .txt
+% Pecah list codes pada semua kemunculan Sep
+pecahPada(_, [], [[]]).
+pecahPada(Sep, [Sep|T], [[]|Rest]) :- !, pecahPada(Sep, T, Rest).
+pecahPada(Sep, [H|T], [[H|Part]|Rest]) :- pecahPada(Sep, T, [Part|Rest]).
+
+% Pecah list codes pada kemunculan Sep pertama saja
+pecahPertama(_, [], [], []).
+pecahPertama(Sep, [Sep|T], [], T) :- !.
+pecahPertama(Sep, [H|T], [H|R1], R2) :- pecahPertama(Sep, T, R1, R2).
+
+removeLastCode([_], []) :- !.
+removeLastCode([H|T], [H|R]) :- removeLastCode(T, R).
+
+% Hapus single quote di awal dan akhir codes jika ada
+stripOuterQuotes([39|Rest], Clean) :- removeLastCode(Rest, Clean), !.
+stripOuterQuotes(Codes, Codes).
+
+stripPrefix([], Rest, Rest).
+stripPrefix([H|PT], [H|AT], Rest) :- stripPrefix(PT, AT, Rest).
+
+mapCodesToAtoms([], []).
+mapCodesToAtoms([Codes|T], [Atom|AT]) :-
+    stripOuterQuotes(Codes, CleanCodes),
+    atom_codes(Atom, CleanCodes),
+    mapCodesToAtoms(T, AT).
+
+mapCodesToKartu([], []).
+mapCodesToKartu([Codes|T], [kartu(W,J)|KT]) :-
+    atom_codes(KartuAtom, Codes),
+    splitDash(KartuAtom, W, J),
+    mapCodesToKartu(T, KT).
+
+splitDash(Atom, W, J) :-
+    atom_codes(Atom, Codes),
+    pecahPertama(45, Codes, WCodes, JCodes),
+    atom_codes(W, WCodes),
+    atom_codes(JAtom, JCodes),
+    (atom_number(JAtom, N) -> J = N ; J = JAtom).
+
+parseListAtom(Atom, Hasil) :-
+    atom_codes(Atom, [91|Rest]),
+    removeLastCode(Rest, InnerCodes),
+    (InnerCodes = [] -> Hasil = []
+    ;
+        pecahPada(44, InnerCodes, Parts),
+        mapCodesToAtoms(Parts, Hasil)
+    ).
+
+parseListKartu(Atom, Hasil) :-
+    atom_codes(Atom, [91|Rest]),
+    removeLastCode(Rest, InnerCodes),
+    (InnerCodes = [] -> Hasil = []
+    ;
+        pecahPada(44, InnerCodes, Parts),
+        mapCodesToKartu(Parts, Hasil)
+    ).
+
 baca_baris_loop(Stream) :-
     at_end_of_stream(Stream), !.
 baca_baris_loop(Stream) :-
@@ -95,62 +156,69 @@ baca_satu_baris(Stream, Acc, Hasil) :-
 parse_baris_ascii(KarakterList) :-
     split_key_value(KarakterList, KeyCodes, ValueCodes),
     atom_codes(Key, KeyCodes),
-    atom_codes(ValueAtom, ValueCodes),
+    stripOuterQuotes(ValueCodes, CleanValueCodes),
+    atom_codes(ValueAtom, CleanValueCodes),
     restore_fakta_ascii(Key, ValueAtom), !.
 
 split_key_value([58|Tail], [], Tail) :- !.
 split_key_value([Head|Tail], [Head|KeyTail], Value) :- split_key_value(Tail, KeyTail, Value).
 
-restore_fakta_ascii(arah_permainan, Value) :- !, retractall(arahPermainan(_)), assertz(arahPermainan(Value)).
-restore_fakta_ascii(warna_aktif, Value) :- !, retractall(warnaActive(_)), assertz(warnaActive(Value)).
+restore_fakta_ascii(arah_permainan, Value) :- !,
+    retractall(arahPermainan(_)), assertz(arahPermainan(Value)).
+
+restore_fakta_ascii(warna_aktif, Value) :- !,
+    retractall(warnaActive(_)), assertz(warnaActive(Value)).
+
 restore_fakta_ascii(discard_top, Value) :- !,
-    split_strip(Value, W, J), retractall(kartuTeratas(_,_)), assertz(kartuTeratas(W, J)).
+    splitDash(Value, W, J),
+    retractall(kartuTeratas(_,_)), assertz(kartuTeratas(W, J)).
+
 restore_fakta_ascii(urutan_pemain, Value) :- !,
-    parse_list_pemain(Value, List), retractall(giliran(_)), assertz(giliran(List)).
+    parseListAtom(Value, List),
+    retractall(giliran(_)), assertz(giliran(List)).
+
 restore_fakta_ascii(giliran, Value) :- !,
     retractall(giliranAktifTemp(_)), assertz(giliranAktifTemp(Value)).
-restore_fakta_ascii(status_uni, Value) :- !,
-    parse_list_pemain(Value, List), retractall(statusUni(_)), assertz(statusUni(List)).
+
+restore_fakta_ascii(status_UNI, Value) :- !,
+    parseListAtom(Value, List),
+    retractall(statusUni(_)), assertz(statusUni(List)).
+
 restore_fakta_ascii(kartu_tersembunyi, Value) :- !,
-    atom_string(Value, Str), split_string(Str, "-", "", [PStr, WStr, JStr]),
-    atom_string(P, PStr), atom_string(W, WStr), atom_string(J, JStr),
-    (atom_number(J, Angka) -> JVal = Angka ; JVal = J),
-    retractall(kartuTersembunyi(P, _)), assertz(kartuTersembunyi(P, kartu(W, JVal))).
+    atom_codes(Value, Codes),
+    pecahPada(45, Codes, [PCodes, WCodes, JCodes]),
+    stripOuterQuotes(PCodes, CleanPCodes),
+    atom_codes(P, CleanPCodes),
+    atom_codes(W, WCodes),
+    atom_codes(JAtom, JCodes),
+    (atom_number(JAtom, N) -> J = N ; J = JAtom),
+    retractall(kartuTersembunyi(P, _)),
+    assertz(kartuTersembunyi(P, kartu(W, J))).
+
+restore_fakta_ascii(kartu_aksi_terakhir, Value) :- !,
+    atom_codes(Value, Codes),
+    pecahPada(45, Codes, [WCodes, JCodes, PCodes]),
+    atom_codes(W, WCodes),
+    atom_codes(JAtom, JCodes),
+    (atom_number(JAtom, N) -> J = N ; J = JAtom),
+    stripOuterQuotes(PCodes, CleanPCodes),
+    atom_codes(Pemain, CleanPCodes),
+    retractall(aksiTerakhir(_,_,_,_)),
+    assertz(aksiTerakhir(W, J, Pemain, 0)).
+
+% Key format: kartu('NamaPemain'):
 restore_fakta_ascii(Key, Value) :-
-    atom_string(Key, KeyStr), string_concat("kartu_", NamaPemainStr, KeyStr), !,
-    atom_string(NamaPemain, NamaPemainStr),
-    parse_list_kartu(Value, ListKartu),
-    retractall(kartudiTangan(NamaPemain, _)), assertz(kartudiTangan(NamaPemain, ListKartu)).
+    atom_codes(Key, KeyCodes),
+    atom_codes('kartu(', PrefixCodes),
+    stripPrefix(PrefixCodes, KeyCodes, [39|Rest]),
+    removeLastCode(Rest, Temp),
+    removeLastCode(Temp, NamaCodes),
+    atom_codes(NamaPemain, NamaCodes), !,
+    parseListKartu(Value, ListKartu),
+    retractall(kartudiTangan(NamaPemain, _)),
+    assertz(kartudiTangan(NamaPemain, ListKartu)).
+
 restore_fakta_ascii(_, _).
-
-split_strip(AtomInput, W, J) :-
-    atom_string(AtomInput, Str), split_string(Str, "-", "", [WStr, JStr]),
-    atom_string(W, WStr), atom_string(J, JStr),
-    (atom_number(J, Angka) -> J = Angka ; true).
-
-parse_list_pemain(AtomList, Hasil) :-
-    atom_string(AtomList, Str), string_concat("[", Tmp1, Str), string_concat(Tmp2, "]", Tmp1),
-    (Tmp2 = "" -> Hasil = []
-    ; split_string(Tmp2, ",", "", StrList), map_string_to_atom(StrList, Hasil)).
-
-map_string_to_atom([], []).
-map_string_to_atom([H|T], [AtomH|AtomT]) :- atom_string(AtomH, H), map_string_to_atom(T, AtomT).
-
-parse_list_kartu(AtomList, Hasil) :-
-    atom_string(AtomList, Str), string_concat("[", Tmp1, Str), string_concat(Tmp2, "]", Tmp1),
-    (Tmp2 = "" -> Hasil = []
-    ; split_string(Tmp2, ",", "", StrList), build_kartu_list(StrList, Hasil)).
-
-build_kartu_list([], []).
-build_kartu_list([H|T], [kartu(W, JVal)|Tail]) :-
-    split_string(H, "-", "", [WStr, JStr]),
-    atom_string(W, WStr), atom_string(J, JStr),
-    (atom_number(J, Angka) -> JVal = Angka ; JVal = J),
-    build_kartu_list(T, Tail).
-
-restoreStateTetap :-
-    retractall(penantangWDF(_)), assertz(penantangWDF(none)),
-    retractall(sudahMainKartu(_)), assertz(sudahMainKartu(false)).
 
 restoreGiliran :-
     (giliranAktifTemp(GiliranAktif) ->
@@ -159,6 +227,12 @@ restoreGiliran :-
         retractall(giliran(_)), assertz(giliran(UrutanRotasi)),
         retractall(giliranAktifTemp(_))
     ; true).
+
+restoreStateTetap :-
+    retractall(penantangWDF(_)),   assertz(penantangWDF(none)),
+    retractall(sudahMainKartu(_)), assertz(sudahMainKartu(false)),
+    retractall(statusBluffWDF(_)), assertz(statusBluffWDF(jujur)),
+    retractall(giliranKe(_)),      assertz(giliranKe(1)).
 
 restoreTumpukan :-
     giliran(ListPemain),
@@ -169,21 +243,18 @@ restoreTumpukan :-
     assertz(tumpukan_kartu(SisaDeck)).
 
 loadGame :-
-    write('Masukkan nama file yang akan dimuat: '),
+    write('Masukkan nama file yang akan dimuat (diakhiri titik): '),
     read(NamaFile),
-    atom_string(NamaFile, NamaStr),
-    string_concat(NamaStr, '.txt', NamaFileExt),
-    atom_string(NamaFileAtom, NamaFileExt),
-    (exists_file(NamaFileAtom) ->
-        open(NamaFileAtom, read, Stream),
+    atom_concat(NamaFile, '.txt', NamaFileAtom),
+    (catch(open(NamaFileAtom, read, Stream), _, fail) ->
         baca_baris_loop(Stream),
         close(Stream),
         restoreGiliran,
         restoreStateTetap,
         restoreTumpukan,
         giliran([GiliranSekarang|_]),
-        write('Status permainan berhasil dimuat dari '), write(NamaFileExt), write('.'), nl,
+        write('Status permainan berhasil dimuat dari '), write(NamaFileAtom), write('.'), nl,
         write('Melanjutkan giliran '), write(GiliranSekarang), write('.'), nl
     ;
-        write('Error: File '), write(NamaFileExt), write(' tidak ditemukan'), nl, fail
+        write('Error: File '), write(NamaFileAtom), write(' tidak ditemukan.'), nl, fail
     ).
